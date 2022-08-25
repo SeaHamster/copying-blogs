aeccc92cfc9473409a691cc592c4a02029b492b0
copying <2221380662@qq.com>
2022-08-25 09:26:57 +0800

    画布代码瀑布修改

--- src/main/resources/static/js/digital_rain.js
+++ src/main/resources/static/js/digital_rain.js
@@ -1,29 +1,30 @@
 window.onload = function(){
     //获取画布对象
-    var canvas = document.getElementById("canvas");
+    const canvas = document.getElementById("canvas");
     //获取画布的上下文
-    var context =canvas.getContext("2d");
-    // var s = window.screen;
-    // var W = canvas.width = s.width;
-    // var H = canvas.height;
+    const context =canvas.getContext("2d");
+    // const s = window.screen;
+    // const W = canvas.width = s.width;
+    // const H = canvas.height;
     //获取浏览器屏幕的宽度和高度
-    var W = window.innerWidth;
-    var H = window.innerHeight;
+    const W = window.innerWidth;
+    const H = window.innerHeight;
     //设置canvas的宽度和高度
     canvas.width = W;
     canvas.height = H;
     //每个文字的字体大小
-    var fontSize = 12;
+    const fontSize = 14;
     //计算列
-    var colunms = Math.floor(W /fontSize);
+    const colunms = Math.floor(W /fontSize);
     //记录每列文字的y轴坐标
-    var drops = [];
+    const drops = [];
     //给每一个文字初始化一个起始点的位置
-    for(var i=0;i<colunms;i++){
+    for(let i=0; i<colunms; i++){
         drops.push(Math.random()*H);
     }
     //运动的文字
-    var str ="WELCOME TO WWW.FUZHIZHONG.COM";
+    const str ="WELCOME TO WWW.FUZHIZHONG.COM 01";
+    // const str = "0123456789";
     //4:fillText(str,x,y);原理就是去更改y的坐标位置
     //绘画的函数
     function draw(){
@@ -39,11 +40,11 @@ window.onload = function(){
         context.fillStyle = randColor();//randColor();可以rgb,hsl, 标准色，十六进制颜色
         // context.fillStyle = "rgb(142,246,13)";
         //写入画布中
-        for(var i=0;i<colunms;i++){
-            var x = i*fontSize;
+        for(let i=0;i<colunms;i++){
+            const x = i*fontSize;
             //下落速度
-            var y = drops[i] * fontSize;
-            var index = Math.floor(Math.random() * str.length);
+            const y = drops[i] * fontSize;
+            const index = Math.floor(Math.random() * str.length);
             context.fillText(str[index],x,y);
             //如果要改变时间，肯定就是改变每次他的起点
             if(y >= canvas.height && Math.random() > 0.99){
@@ -54,14 +55,14 @@ window.onload = function(){
     }
     function randColor(){
         //随机颜色
-        var r = Math.floor(Math.random() * 256);
-        var g = Math.floor(Math.random() * 156)+80;
-        var b = Math.floor(Math.random() * 256);
+        // const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 156)+80;
+        // const b = Math.floor(Math.random() * 256);
         // return "rgb("+r+","+g+","+b+")";
         //绿色随机
         return "rgb(0,"+g+",0)"
     }
     draw();
-    setInterval(draw,35);
+    setInterval(draw,36);
 };
 
